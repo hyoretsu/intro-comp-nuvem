1d9e3c0218e4e3d25c33f7ea3303ab884c7f576d
Aran Leite <hyoretsu@gmail.com>
2024-12-05 22:50:55 -0300

    Better docs for GET /media and POST /media

--- backend/infra/http/elysia/controllers/Media.ts
+++ backend/infra/http/elysia/controllers/Media.ts
@@ -23,67 +23,140 @@ export const MediaController = new Elysia()
 					tags: ["Media"],
 				},
 				query: t.Object({
-					category: t.Optional(t.Enum(Category)),
-					title: t.Optional(t.String()),
+					category: t.Optional(t.Enum(Category, { description: "Media category." })),
+					title: t.Optional(t.String({ description: "Media title filter." })),
 				}),
 				// response: t.Array(t.Union([LiteraryWork, Movie, Video])),
-				response: t.Array(t.Record(t.String(), t.Any())),
+				response: t.Array(
+					t.Object({
+						id: t.String(),
+						title: t.Nullable(t.Record(t.String(), t.Union([t.String(), t.Array(t.String())]))),
+						releaseDate: t.Nullable(t.Date()),
+					}),
+					{ description: "List of media." },
+				),
 			})
 			.post("/", ({ body, createMedia }) => createMedia.execute(body), {
 				detail: {
 					tags: ["Media"],
 				},
-				body: t.Union([
-					t.Object(
-						{
-							category: t.Literal(Category.CHAPTER),
-							number: t.Number(),
-							pages: t.Optional(t.Integer()),
-							releaseDate: t.Optional(t.Date()),
-							sourceId: t.String(),
-							title: t.Optional(t.Record(t.String(), t.String())),
-						},
-						{ additionalProperties: false },
-					),
-					t.Object(
-						{
-							category: t.Literal(Category.LITERARY_WORK),
-							currentChapters: t.Optional(t.Integer()),
-							ongoing: t.Optional(t.Boolean()),
-							synopsis: t.Optional(t.Record(t.String(), t.String())),
-							tags: t.Optional(t.Array(t.String())),
-							title: t.Record(t.String(), t.Array(t.String())),
-							type: t.Enum(LiteraryWorkType),
-						},
-						{ additionalProperties: false },
-					),
-					t.Object(
-						{
-							category: t.Literal(Category.MOVIE),
-							duration: t.Optional(t.String()),
-							releaseDate: t.Optional(t.Date()),
-							title: t.Record(t.String(), t.Array(t.String())),
-						},
-						{ additionalProperties: false },
-					),
-					t.Object(
-						{
-							category: t.Literal(Category.VIDEO),
-							duration: t.Optional(t.String()),
-							link: t.Optional(t.String({ format: "uri" })),
-							title: t.Record(t.String(), t.String()),
-						},
-						{ additionalProperties: false },
-					),
-					t.Object(
-						{
-							category: t.Literal(Category.VIDEO_GAME),
-							title: t.Record(t.String(), t.Array(t.String())),
-						},
-						{ additionalProperties: false },
-					),
-				]),
-				response: t.String(),
+				body: t.Union(
+					[
+						t.Object(
+							{
+								category: t.Literal(Category.CHAPTER),
+								number: t.Number({ description: "Chapter's number in the series." }),
+								pages: t.Optional(t.Integer({ description: "Number of pages in the chapter." })),
+								releaseDate: t.Optional(t.Date({ description: "Chapter's original release date." })),
+								sourceId: t.String({ description: "The literary work's ID." }),
+								title: t.Optional(
+									t.Record(t.String(), t.String(), {
+										description: "Titles (array) by language code.",
+									}),
+								),
+							},
+							{ additionalProperties: false },
+						),
+						t.Object(
+							{
+								category: t.Literal(Category.LITERARY_WORK),
+								currentChapters: t.Optional(
+									t.Integer({
+										description:
+											"Number of chapters already released. Automatically creates them without title and pages.",
+									}),
+								),
+								ongoing: t.Optional(t.Boolean({ description: "Is the work not finished yet?" })),
+								synopsis: t.Optional(
+									t.Record(t.String(), t.String(), { description: "Synopsis by language code." }),
+								),
+								tags: t.Optional(t.Array(t.String(), { description: "Category/genre tags" })),
+								title: t.Record(t.String(), t.Array(t.String()), {
+									description: "Titles (array) by language code.",
+								}),
+								type: t.Enum(LiteraryWorkType),
+							},
+							{ additionalProperties: false },
+						),
+						t.Object(
+							{
+								category: t.Literal(Category.MOVIE),
+								duration: t.Optional(t.String({ description: "ISO 8601 duration.", examples: ["PT_H_M_S"] })),
+								releaseDate: t.Optional(
+									t.Date({
+										examples: ["2024-12-25T00:00:00.000-03:00"],
+									}),
+								),
+								title: t.Record(t.String(), t.Array(t.String()), {
+									description: "Titles (array) by language code.",
+								}),
+							},
+							{ additionalProperties: false },
+						),
+						t.Object(
+							{
+								category: t.Literal(Category.VIDEO),
+								link: t.String({ description: "A youtube URL.", format: "uri" }),
+							},
+							{ additionalProperties: false },
+						),
+						t.Object(
+							{
+								category: t.Literal(Category.VIDEO_GAME),
+								title: t.Record(t.String(), t.Array(t.String()), {
+									description: "Title by language code.",
+								}),
+							},
+							{ additionalProperties: false },
+						),
+					],
+					{
+						examples: [
+							{
+								cateogry: "chapter",
+								number: 1,
+								pages: 12,
+								releaseDate: "2024-12-25T00:00:00-03:00",
+								sourceId: "27914233761075200",
+								title: {
+									en: ["The beginning of everything"],
+								},
+							},
+							{
+								cateogry: "literary_work",
+								currentChapters: 1,
+								ongoing: false,
+								synopsis: {
+									en: "A story about everything.",
+								},
+								tags: ["Cultivation"],
+								title: {
+									en: ["I don't know"],
+								},
+								type: "manhua",
+							},
+							{
+								cateogry: "movie",
+								duration: "PT2H5M30S",
+								releaseDate: "2024-12-25T00:00:00-03:00",
+								title: {
+									en: ["Awesome movie"],
+								},
+							},
+							{
+								cateogry: "video",
+								link: "https://www.youtube.com/watch?v=1234",
+							},
+							{
+								cateogry: "video_game",
+								title: {
+									en: ["The game"],
+								},
+							},
+						],
+					},
+				),
+				response: t.String({ description: "Created media ID." }),
 			})
 			.post("/track", ({ body, query, trackMedia }) => trackMedia.execute({ ...body, ...query }), {
 				detail: {
